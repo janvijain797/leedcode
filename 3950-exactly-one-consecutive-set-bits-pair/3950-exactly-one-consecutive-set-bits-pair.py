8e11ba1469d385f1ba44927390ba68b3c045a53f
Janvi Jain <janvijain797@gmail.com>
2026-06-14 07:45:34 +0530

    Time: 3 ms (23.89%), Space: 19.5 MB (17.38%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.py
@@ -0,0 +1,12 @@
+class Solution:
+    def consecutiveSetBits(self, n: int) -> bool:
+        b = bin(n)[2:]
+        count = 0 
+        for i in range(len(b) - 1 ):
+            if b[i] == '1' and b[i+1] == '1':
+                count +=1 
+        return count == 1 
+        
+            
+            
+    
