b07c4cb490786d36669ad3a4fa28025f96084079
Janvi Jain <janvijain797@gmail.com>
2026-07-15 09:01:28 +0530

    Time: 7 ms (86.19%), Space: 20.5 MB (67.34%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,13 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        arr = [] 
-        sorted_arr = sorted(nums)
-        for i in range(1,n):
-            if sorted_arr [i-1] == sorted_arr[i]:
-                duplicate = sorted_arr[i]
-                break  
-        expacted_sum = n*(n+1)//2
-        actual_sum = sum(nums)
-        missing_num = expacted_sum - (actual_sum -duplicate)
-        return [duplicate,missing_num ]
+        expected_sum = n*(n+1)//2 
+        exp_sq_sum = n*(n+1)*(2*n+1)//6
+        actual_sum = 0 
+        actual_sq_sum = 0 
+        for num in nums:
+            actual_sum += num
+            actual_sq_sum += num*num
+        x = expected_sum - actual_sum 
+        y = exp_sq_sum - actual_sq_sum 
+        sumAB = y//x
+        missing = (x+sumAB)//2
+        repeating = missing - x 
+        return(repeating, missing)
+      
